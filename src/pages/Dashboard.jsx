997754e3a7b1a83e3fdcd90e6662beeecb33113d
leadkastos <clientcare@leadkast.com>
2026-04-14 10:35:41 -0500

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,8 +3,11 @@ import AppLayout from '../components/AppLayout'
 import MorningBriefCard from '../components/MorningBriefCard'
 import DailyWrapCard from '../components/DailyWrapCard'
 import DashboardCard from '../components/DashboardCard'
+import SmartMoneyCard from '../components/SmartMoneyCard'
 import { dashboardCards } from '../data/mockData'
+
 export default function Dashboard() {
+  const filteredCards = dashboardCards.filter(function(card) { return card.id !== 'youtube' })
   return (
     <AppLayout>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-6">
@@ -17,9 +20,10 @@ export default function Dashboard() {
         </h2>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
-        {dashboardCards.map((card) => (
-          <DashboardCard key={card.id} card={card} />
-        ))}
+        {filteredCards.map(function(card) {
+          return <DashboardCard key={card.id} card={card} />
+        })}
+        <SmartMoneyCard />
       </div>
     </AppLayout>
   )
