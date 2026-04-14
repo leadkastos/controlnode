522caffd0904087c62d4249da3982b3729c5769b
leadkastos <clientcare@leadkast.com>
2026-04-14 13:43:46 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
+import { PriceProvider } from './contexts/PriceContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <PriceProvider>
+        <App />
+      </PriceProvider>
     </AuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
