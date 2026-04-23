0b227d8d5f21354d2937e14859cbb0d948fe4e5f
leadkastos <clientcare@leadkast.com>
2026-04-23 17:50:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,7 @@ import ETFFlows from './pages/ETFFlows'
 import Watchlist from './pages/Watchlist'
 import YouTubeIntel from './pages/YouTubeIntel'
 import SmartMoneyFlow from './pages/SmartMoneyFlow'
-import MarketChatter from './pages/MarketChatter'
-import BitcoinDashboard from './pages/BitcoinDashboard'
+import MarketNews from './pages/MarketNews'
 import { Account, Billing, Settings } from './pages/AccountPages'
 import {
   Admin,
@@ -61,8 +60,8 @@ export default function App() {
         <Route path="/watchlist" element={<ProtectedRoute><Watchlist /></ProtectedRoute>} />
         <Route path="/youtube-intel" element={<ProtectedRoute><YouTubeIntel /></ProtectedRoute>} />
         <Route path="/smart-money-flow" element={<ProtectedRoute><SmartMoneyFlow /></ProtectedRoute>} />
-        <Route path="/market-chatter" element={<ProtectedRoute><MarketChatter /></ProtectedRoute>} />
-        <Route path="/bitcoin" element={<ProtectedRoute><BitcoinDashboard /></ProtectedRoute>} />
+        <Route path="/market-news" element={<ProtectedRoute><MarketNews /></ProtectedRoute>} />
+        <Route path="/market-chatter" element={<ProtectedRoute><MarketNews /></ProtectedRoute>} />
         <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
         <Route path="/billing" element={<ProtectedRoute><Billing /></ProtectedRoute>} />
         <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
